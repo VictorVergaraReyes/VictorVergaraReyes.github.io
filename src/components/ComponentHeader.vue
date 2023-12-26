f58d5fc85da808023c266a90d0720c009d9618df
Victor Vergara Reyes <vvergara@ahorra.io>
2023-12-25 22:27:59 -0600

    bootstrap

---
+++ src/components/ComponentHeader.vue
@@ -0,0 +1,17 @@
+<script setup>
+    
+</script>
+<template>
+    <div class="d-flex flex-row container-fluid important bg-primary-subtle">
+        <h2>About me</h2>
+        <h2>Projects</h2>
+        <h2>Contact me</h2>
+    </div>
+</template>
+
+<style>
+.box{
+    display: flex;
+    flex-direction: column;
+}
+</style>
