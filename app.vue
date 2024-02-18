1ab4b96443bb65557508bf21402320e9aa2f52b3
DESKTOP-PG8I4AQ\vic <vvergara@ahorra.io>
2024-02-17 22:58:35 -0600

    tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+    <div class="text-3xl font-bold underline">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia, illum? In voluptatum ut pariatur voluptatem quisquam porro repudiandae quod sit ullam. Sint doloremque praesentium quam doloribus perspiciatis vero ad iusto.</div>
   </div>
 </template>
