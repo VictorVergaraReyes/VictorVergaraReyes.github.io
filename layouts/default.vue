098f9b239852d67b39c0022c8d19885a2dd0ccf3
vicvergara <vvergara@ahorra.io>
2024-06-06 09:57:18 -0600

    bootstrap

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="text-bg-primary">
+        <div id="banner" class="bg">
+            BANNER
+        </div>
+        <div id="projects">
+            projects
+        </div>
+        <div>
+            
+        </div>
+        <div>
+            
+        </div>
+    </div>
+</template>
