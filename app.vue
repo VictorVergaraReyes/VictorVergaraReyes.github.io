3de9242063a6a26ac448166b798c2f63130ecf4a
vicvergara <vvergara@ahorra.io>
2024-06-06 05:50:21 -0600

    re starting nuxt

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <!-- <NuxtWelcome /> -->
-    <div class="text-3xl font-bold underline">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia, illum? In voluptatum ut pariatur voluptatem quisquam porro repudiandae quod sit ullam. Sint doloremque praesentium quam doloribus perspiciatis vero ad iusto.</div>
+    <NuxtWelcome />
   </div>
 </template>
