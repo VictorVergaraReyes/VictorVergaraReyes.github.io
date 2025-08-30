6eed9ebeb20019e787f0b877d53471252a95ce17
VicVR <victorvergarareyes2002@gmail.com>
2025-08-29 21:58:28 -0600

    second attempt to deploy react

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './', // Para GitHub Actions
+  base: 'https://victorvergarareyes.github.io/VictorVergaraReyes.github.io', // Para GitHub Actions
   build: {
     outDir: 'dist'
   }
